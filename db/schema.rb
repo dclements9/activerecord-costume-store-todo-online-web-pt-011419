4d1d1e4dcd6086e7c0d97159ee388d0abe818cdf
dclements9 <dclements@protonmail.com>
2019-04-02 10:03:34 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "costume_inventory"
     t.integer "num_of_employees"
     t.boolean "still_in_business"
-    t.time "opening_time"
-    t.time "closing_time"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.date "opening_date"
     t.date "closing_date"
-    t.string "long_description"
+    t.string "description"
   end
 
 end
