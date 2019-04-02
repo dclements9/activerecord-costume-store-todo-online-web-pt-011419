7a42ee7150f264ae90cf53f90782afbaecb19875
dclements9 <dclements@protonmail.com>
2019-04-02 09:44:25 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores
+    def change
+        create_table :costume_stores do |column|
+            column.string :name
+            column.string :location
+            column.integer :costume_inventory
+            column.integer :employees
+            column.boolean :in_business
+            column.time :opening_time
+            column.time :closing_time
+        end 
+    end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses
+    def change
+        create_table :haunted_houses do |column|
+            column.string :name
+            column.string :location
+            column.string :theme
+            column.float :price
+            column.boolean :family_friendly
+            column.date :opening_date
+            column.date :closing_date
+            column.string :long_description
+        end
+    end
+end
